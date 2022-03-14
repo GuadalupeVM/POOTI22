cd52d0a5fdfbe5466e7d30fcc06699616ac8ce0e
GuadalupeVM <97322599+GuadalupeVM@users.noreply.github.com>
2022-03-14 07:48:11 -0600

    Create progrma_tarea.py

---
+++ progrma_tarea.py
@@ -0,0 +1,24 @@
+letra = input("letra: ")
+if letra=='a':
+    lista=[[0,1,1,1,0],[0,1,0,1,0],[0,1,1,1,0],[0,1,0,1,0],[0,1,0,1,0]]
+    for fila in lista:
+        print(fila)
+elif letra=='e':
+    lista=[[1,1,1,0,0],[1,0,0,0,0],[1,1,1,0,0],[1,0,0,0,0],[1,1,1,0,0]]
+    for fila in lista:
+        print(fila)
+
+elif letra=='i':
+    lista=[[0,1,1,1,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,1,1,1,0]]
+    for fila in lista:
+        print(fila)
+
+elif letra=='o':
+    lista=[[0,1,1,1,0],[0,1,0,1,0],[0,1,0,1,0],[0,1,0,1,0],[0,1,1,1,0]]
+    for fila in lista:
+        print(fila)
+
+elif letra=='u':
+    lista=[[0,1,0,1,0],[0,1,0,1,0],[0,1,0,1,0],[0,1,0,1,0],[0,1,1,1,0]]
+    for fila in lista:
+        print(fila)
